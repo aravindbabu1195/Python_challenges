231c798d79d4d5dc0434c4dec885a0f9a8977516
Aravind Babu <aravindbabu1195@gmail.com>
2023-05-06 12:51:25 +0530

    Challenge 1&2 updated

---
+++ challenge1.py
@@ -0,0 +1,18 @@
+print("Names of Movies")
+Movies=["jhon wick","interstellar","Inception","Shazam","TENET","uncharted","Shang_chi","Eternals","Zack snyder's","WW84"]
+print(Movies)
+
+print("Romoving the 5th element from the movie list")
+Movies.pop(4)
+print(Movies)
+
+print("Inserting movie director name to 4th index position")
+Movies.insert(4,"Mr.Christopher Nolan")
+print(Movies)
+
+print("List out the 4th element in the list")
+print(Movies[3])
+
+print("Adding more movie names")
+Movies.append("Dark Knight")
+print(Movies)
---
+++ challenge2.py
@@ -0,0 +1,9 @@
+num=[10,20,30,40]
+num1=[10,20,30,40]
+res=[]
+for i in range(len(num)):
+    res.append(num[i]+num1[i])
+print(res)
+
+for j in range(7):
+    print("I Love My India ")
